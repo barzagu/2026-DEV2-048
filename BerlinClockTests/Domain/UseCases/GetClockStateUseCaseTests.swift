dc1d08f29d9914b9fd4d1f00683e6e94a8ea5ca7
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 16:35:29 +0100

    test: (red) add inputs and outputs and call sut method

--- BerlinClockTests/Domain/UseCases/GetClockStateUseCaseTests.swift
+++ BerlinClockTests/Domain/UseCases/GetClockStateUseCaseTests.swift
@@ -17,4 +17,24 @@ struct GetClockStateUseCaseTests {
     )
   }
 
+  @Test
+  func test_get_state_emits_states_when_receiving_times() {
+    let inputs: [Time] = (1...4).map { _ in
+        .init(hours: 23, minutes: 59, seconds: 59)
+    }
+    let outputs: [ClockState] = []
+    let expectedOutputs: [ClockState] = (1...4).map { _ in
+      .init(
+        time: .init(hours: 23, minutes: 59, seconds: 59),
+        fiveHoursBlocks: [true, true, true, true],
+        oneHourBlocks: [true, true, true, false],
+        fiveMinutesBlocks: [true, true, true, true, true, true, true, true, true, true, true],
+        oneMinuteBlocks: [true, true, true, true],
+        seconds: false
+      )
+    }
+
+    let cancellable = sut.getState().sink { _ in }
+  }
+
 }
