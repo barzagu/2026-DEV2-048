6d5fa96b59ef6061c51c34010bca689015567c64
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 17:31:35 +0100

    feat: (green) add UIClockState struct

---
+++ BerlinClock/Presentation/Models/UIClockState.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct UIClockState {
+  let time: String
+  let fiveHoursBlocks: [Color]
+  let oneHourBlocks: [Color]
+  let fiveMinutesBlocks: [Color]
+  let oneMinuteBlocks: [Color]
+  let seconds: Color
+}
