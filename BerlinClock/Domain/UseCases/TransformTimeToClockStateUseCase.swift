b1a05f0df5da7d845504773b6bcfcb9a0eff0a00
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 15:46:55 +0100

    feat: (green) implement the transform method in the use case

--- BerlinClock/Domain/UseCases/TransformTimeToClockStateUseCase.swift
+++ BerlinClock/Domain/UseCases/TransformTimeToClockStateUseCase.swift
@@ -4,11 +4,39 @@ class TransformTimeToClockStateUseCase {
   func transform(_ time: Time) -> ClockState {
     .init(
       time: time,
-      fiveHoursBlocks: [],
-      oneHourBlocks: [],
-      fiveMinutesBlocks: [],
-      oneMinuteBlocks: [],
-      seconds: true
+      fiveHoursBlocks: calculateFiveHoursBlocks(from: time),
+      oneHourBlocks: calculateOneHourBlocks(from: time),
+      fiveMinutesBlocks: calculateFiveMinutesBlocks(from: time),
+      oneMinuteBlocks: calculateOneMinuteBlocks(from: time),
+      seconds: calculateSeconds(from: time)
     )
+  }
+
+  private func calculateFiveHoursBlocks(from time: Time) -> [Bool] {
+    let blocks = [false, false, false, false]
+    let isActive = { (index: Int) in index < (time.hours / 5) }
+    return blocks.indices.map { isActive($0) }
+  }
+
+  private func calculateOneHourBlocks(from time: Time) -> [Bool] {
+    let blocks = [false, false, false, false]
+    let isActive = { (index: Int) in index < (time.hours % 5) }
+    return blocks.indices.map { isActive($0) }
+  }
+
+  private func calculateFiveMinutesBlocks(from time: Time) -> [Bool] {
+    let blocks = [false, false, false, false, false, false, false, false, false, false, false]
+    let isActive = { (index: Int) in index < (time.minutes / 5) }
+    return blocks.indices.map { isActive($0) }
+  }
+
+  private func calculateOneMinuteBlocks(from time: Time) -> [Bool] {
+    let blocks = [false, false, false, false]
+    let isActive = { (index: Int) in index < (time.minutes % 5) }
+    return blocks.indices.map { isActive($0) }
+  }
+
+  private func calculateSeconds(from time: Time) -> Bool {
+    time.seconds.isMultiple(of: 2)
   }
 }
