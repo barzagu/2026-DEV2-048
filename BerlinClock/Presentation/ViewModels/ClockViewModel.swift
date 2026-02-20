71c70142287494306f19b6af733c0e687dce2199
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 20:17:58 +0100

    refactor: clean up transform method
    Move individual calculations to independent methods to clean it up a
    bit.

--- BerlinClock/Presentation/ViewModels/ClockViewModel.swift
+++ BerlinClock/Presentation/ViewModels/ClockViewModel.swift
@@ -20,14 +20,38 @@ class ClockViewModel {
 
   private func transform(_ state: ClockState) -> UIClockState {
     .init(
-      time: String(format: "%02d:%02d:%02d", state.time.hours, state.time.minutes, state.time.seconds),
-      fiveHoursBlocks: state.fiveHoursBlocks.map { $0 ? .red : .clear },
-      oneHourBlocks: state.oneHourBlocks.map { $0 ? .red : .clear },
-      fiveMinutesBlocks: state.fiveMinutesBlocks.enumerated().map { (index, block) in
-        block ? ((index + 1) % 3 == 0 ? .red : .yellow) : .clear
-      },
-      oneMinuteBlocks: state.oneMinuteBlocks.map { $0 ? .yellow : .clear },
-      seconds: state.seconds ? .yellow : .clear
+      time: calculateTime(from: state),
+      fiveHoursBlocks: calculateFiveHoursBlocks(from: state),
+      oneHourBlocks: calculateOneHourBlocks(from: state),
+      fiveMinutesBlocks: calculateFiveMinutesBlocks(from: state),
+      oneMinuteBlocks: calculateOneMinuteBlocks(from: state),
+      seconds: calculateSeconds(from: state)
     )
+  }
+
+  private func calculateTime(from state: ClockState) -> String {
+    String(format: "%02d:%02d:%02d", state.time.hours, state.time.minutes, state.time.seconds)
+  }
+
+  private func calculateFiveHoursBlocks(from state: ClockState) -> [Color] {
+    state.fiveHoursBlocks.map { $0 ? .red : .clear }
+  }
+
+  private func calculateOneHourBlocks(from state: ClockState) -> [Color] {
+    state.oneHourBlocks.map { $0 ? .red : .clear }
+  }
+
+  private func calculateFiveMinutesBlocks(from state: ClockState) -> [Color] {
+    state.fiveMinutesBlocks.enumerated().map { (index, block) in
+      block ? ((index + 1) % 3 == 0 ? .red : .yellow) : .clear
+    }
+  }
+
+  private func calculateOneMinuteBlocks(from state: ClockState) -> [Color] {
+    state.oneMinuteBlocks.map { $0 ? .yellow : .clear }
+  }
+
+  private func calculateSeconds(from state: ClockState) -> Color {
+    state.seconds ? .yellow : .clear
   }
 }
