c5c2bbe4eee38f63ca117bb445f8525cd621a2e4
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 14:53:33 +0100

    test: (red) add input, output, and expectedOutput value arrays

--- BerlinClockTests/Domain/UseCases/TransformTimeToClockStateUseCaseTests.swift
+++ BerlinClockTests/Domain/UseCases/TransformTimeToClockStateUseCaseTests.swift
@@ -10,4 +10,15 @@ struct TransformTimeToClockStateUseCaseTests {
     sut = TransformTimeToClockStateUseCase()
   }
 
+  @Test
+  func test_transforming_time_to_clock_states() {
+    let inputValues: [Time] = [
+      .init(hours: 0, minutes: 0, seconds: 0),
+      .init(hours: 10, minutes: 10, seconds: 10),
+      .init(hours: 23, minutes: 59, seconds: 59)
+    ]
+    let outputValues: [ClockState] = []
+    let expectedOutputValues: [ClockState] = []
+  }
+
 }
