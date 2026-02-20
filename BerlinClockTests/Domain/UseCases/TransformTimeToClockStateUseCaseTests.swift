afdef78a3050f3e4296cbe12b7582ad73242ee71
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 15:08:09 +0100

    test: (red) add expected output values to test

--- BerlinClockTests/Domain/UseCases/TransformTimeToClockStateUseCaseTests.swift
+++ BerlinClockTests/Domain/UseCases/TransformTimeToClockStateUseCaseTests.swift
@@ -18,7 +18,32 @@ struct TransformTimeToClockStateUseCaseTests {
       .init(hours: 23, minutes: 59, seconds: 59)
     ]
     let outputValues: [ClockState] = []
-    let expectedOutputValues: [ClockState] = []
+    let expectedOutputValues: [ClockState] = [
+      .init(
+        time: .init(hours: 0, minutes: 0, seconds: 0),
+        fiveHoursBlocks: [false, false, false, false],
+        oneHourBlocks: [false, false, false, false],
+        fiveMinutesBlocks: [false, false, false, false, false, false, false, false, false, false, false],
+        oneMinuteBlocks: [false, false, false, false],
+        seconds: true
+      ),
+      .init(
+        time: .init(hours: 10, minutes: 10, seconds: 10),
+        fiveHoursBlocks: [true, true, false, false],
+        oneHourBlocks: [false, false, false, false],
+        fiveMinutesBlocks: [true, true, false, false, false, false, false, false, false, false, false],
+        oneMinuteBlocks: [false, false, false, false],
+        seconds: true
+      ),
+      .init(
+        time: .init(hours: 23, minutes: 59, seconds: 59),
+        fiveHoursBlocks: [true, true, true, true],
+        oneHourBlocks: [true, true, true, false],
+        fiveMinutesBlocks: [true, true, true, true, true, true, true, true, true, true, true],
+        oneMinuteBlocks: [true, true, true, true],
+        seconds: false
+      )
+    ]
   }
 
 }
