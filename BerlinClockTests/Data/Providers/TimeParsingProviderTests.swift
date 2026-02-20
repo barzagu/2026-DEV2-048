add32d6a78cde62560e709aa173ba57cb07f3cba
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 13:33:21 +0100

    test: (red) add time expectation

--- BerlinClockTests/Data/Providers/TimeParsingProviderTests.swift
+++ BerlinClockTests/Data/Providers/TimeParsingProviderTests.swift
@@ -15,7 +15,10 @@ struct TimeParsingProviderTests {
   @Test
   func test_parsing_time_from_date() {
     let date = calendar.date(from: .init(hour: 23, minute: 59, second: 59))!
+    let expectedTime = Time(hours: 23, minutes: 59, seconds: 59)
     let time: Time = sut.parse(from: date)
+
+    #expect(time == expectedTime)
   }
 
 }
