af198d9669ef96ec30a4ebd01a37944dcd8396d2
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 19:53:08 +0100

    test: (red) add test with inputs, outputs, and expected outputs
    squash

--- BerlinClockTests/Presentation/ViewModels/ClockViewModelTests.swift
+++ BerlinClockTests/Presentation/ViewModels/ClockViewModelTests.swift
@@ -1,3 +1,4 @@
+import SwiftUI
 import Testing
 
 @testable import BerlinClock
@@ -8,6 +9,63 @@ struct ClockViewModelTests {
 
   init() {
     sut = ClockViewModel()
+  }
+
+  @Test
+  func test_start_clock_emits_ui_states_when_receiving_clock_states() {
+    let inputs: [ClockState] = [
+      .init(
+        time: .init(hours: 0, minutes: 0, seconds: 0),
+        fiveHoursBlocks: [false, false, false, false],
+        oneHourBlocks: [false, false, false, false],
+        fiveMinutesBlocks: [false, false, false, false, false, false, false, false, false, false, false],
+        oneMinuteBlocks: [false, false, false, false],
+        seconds: true
+      ),
+      .init(
+        time: .init(hours: 10, minutes: 10, seconds: 10),
+        fiveHoursBlocks: [true, true, false, false],
+        oneHourBlocks: [false, false, false, false],
+        fiveMinutesBlocks: [true, true, false, false, false, false, false, false, false, false, false],
+        oneMinuteBlocks: [false, false, false, false],
+        seconds: true
+      ),
+      .init(
+        time: .init(hours: 23, minutes: 59, seconds: 59),
+        fiveHoursBlocks: [true, true, true, true],
+        oneHourBlocks: [true, true, true, false],
+        fiveMinutesBlocks: [true, true, true, true, true, true, true, true, true, true, true],
+        oneMinuteBlocks: [true, true, true, true],
+        seconds: false
+      )
+    ]
+    var outputs: [UIClockState] = []
+    let expectedOutputs: [UIClockState] = [
+      .init(
+        time: "00:00:00",
+        fiveHoursBlocks: [.clear, .clear, .clear, .clear],
+        oneHourBlocks: [.clear, .clear, .clear, .clear],
+        fiveMinutesBlocks: [.clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear],
+        oneMinuteBlocks: [.clear, .clear, .clear, .clear],
+        seconds: .yellow
+      ),
+      .init(
+        time: "10:10:10",
+        fiveHoursBlocks: [.red, .red, .clear, .clear],
+        oneHourBlocks: [.clear, .clear, .clear, .clear],
+        fiveMinutesBlocks: [.yellow, .yellow, .clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear, .clear],
+        oneMinuteBlocks: [.clear, .clear, .clear, .clear],
+        seconds: .yellow
+      ),
+      .init(
+        time: "23:59:59",
+        fiveHoursBlocks: [.red, .red, .red, .red],
+        oneHourBlocks: [.red, .red, .red, .clear],
+        fiveMinutesBlocks: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow],
+        oneMinuteBlocks: [.yellow, .yellow, .yellow, .yellow],
+        seconds: .clear
+      )
+    ]
   }
 
 }
