f2f6f5ab540a410f767cc93a86f3925bdb819cb5
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 14:44:27 +0100

    test: (red) add TransformTimeToClockStateUseCaseTests test file

---
+++ BerlinClockTests/Domain/UseCases/TransformTimeToClockStateUseCaseTests.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import BerlinClock
+
+struct TransformTimeToClockStateUseCaseTests {
+
+  let sut: TransformTimeToClockStateUseCase!
+
+  init() {
+    sut = TransformTimeToClockStateUseCase()
+  }
+
+}
