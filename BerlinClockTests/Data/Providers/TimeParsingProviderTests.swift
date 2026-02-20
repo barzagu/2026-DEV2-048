11f818a27da3cd2df31351d2c013ea8bfdd87452
2026-DEV2-048 <2026-DEV2-048@gmail.com>
2026-02-20 13:28:10 +0100

    test: (red) add test to parse time objects from date objects

--- BerlinClockTests/Data/Providers/TimeParsingProviderTests.swift
+++ BerlinClockTests/Data/Providers/TimeParsingProviderTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Testing
 
 @testable import BerlinClock
@@ -5,9 +6,16 @@ import Testing
 struct TimeParsingProviderTests {
 
   let sut: TimeParsingProvider!
+  let calendar = Calendar.current
 
   init() {
     sut = TimeParsingProvider()
+  }
+
+  @Test
+  func test_parsing_time_from_date() {
+    let date = calendar.date(from: .init(hour: 23, minute: 59, second: 59))!
+    let time = sut.parse(from: date)
   }
 
 }
